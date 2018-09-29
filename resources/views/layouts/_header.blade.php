474b18764012f45ae0fab4fa6d7ab37d27a3af80
Nata <nataprawiraf@gmail.com>
2018-09-29 22:11:34 +0700

    minor edit

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,17 +3,33 @@
 </div>
 
 <div id="left-blob" class="super super-middle special-narrow-left nav-b">
-    <img src="{{ asset('img/blob_purple.png') }}" alt="menu">
+    <img onClick="fullpage_api.moveSlideLeft()" src="{{ asset('img/blob_purple.png') }}" alt="menu">
 </div>
 
 <div id="right-blob" class="super super-middle special-narrow-right nav-b">
-    <img src="{{ asset('img/blob_purple.png') }}" alt="menu">
+    <img onClick="fullpage_api.moveSlideRight()" src="{{ asset('img/blob_purple.png') }}" alt="menu">
 </div>
 
-<div id="menu" class="super super-narrow-bottom special-narrow-left m-l">
+<div id="menu" onClick="openNav()" class="super super-narrow-bottom special-narrow-left m-l">
     <img src="{{ asset('img/menu.png') }}" alt="menu">
 </div>
 
-<div id="menu" class="super super-narrow-top special-narrow-right m-r">
+<div id="menu" onClick="openNav()" class="super super-narrow-top special-narrow-right m-r">
     <img src="{{ asset('img/menu.png') }}" alt="menu">
 </div>
+
+<div id="myNav" class="overlay">
+
+    <!-- Button to close the overlay navigation -->
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  
+    <!-- Overlay content -->
+    <div class="overlay-content">
+      <a href="javascript:void(0)" onclick="fullpage_api.moveTo('ticket', 5);" class="subtitle">Community</a>
+      <a href="javascript:void(0)" onclick="fullpage_api.moveTo(2);" class="subtitle" >Bazaar</a>
+      <a onclick="fullpage_api.moveTo(3)" class="subtitle" href="javascript:void(0)">Exhibition</a>
+      <a onclick="fullpage_api.moveTo(4)" class="subtitle" href="javascript:void(0)">Music</a>
+    </div>
+  
+  </div>
+  
